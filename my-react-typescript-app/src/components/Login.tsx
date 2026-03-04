92d6708f8281fd748856599e94621138267fecaf
FelixWahyu <felixwahyusejati89@gmail.com>
2026-03-04 13:50:37 +0700

    Update belajar membuat form handler dan validation

--- my-react-typescript-app/src/components/Login.tsx
+++ my-react-typescript-app/src/components/Login.tsx
@@ -1,12 +1,60 @@
+import { useRef, useState } from "react";
 import { Link } from "react-router";
 
+type FormLogin = {
+  username: string;
+  password: string;
+};
+
+type ErrorsMsg = Partial<FormLogin>;
+
+const validate = (values: FormLogin): ErrorsMsg => {
+  const errors: ErrorsMsg = {};
+
+  if (!values.username) {
+    errors.username = "Username wajib diisi!";
+  } else if (values.username.length < 3) {
+    errors.username = "Username minimal 3 karakter!";
+  } else if (values.username.length > 20) {
+    errors.username = "Username maksimal 20 karakter!";
+  }
+
+  if (!values.password) {
+    errors.password = "Password wajib diisi!";
+  } else if (values.password.length < 6) {
+    errors.password = "Password minimal 6 karakter!";
+  }
+
+  return errors;
+};
+
 const LoginPage = () => {
+  const [error, setError] = useState<ErrorsMsg>({});
+
+  // uncontrolled component/form input
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
   const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const username = formData.get("username");
-    const password = formData.get("password");
-    console.log({ username, password });
+    // const formData = new FormData(e.currentTarget);
+    // const username = formData.get("username") as string;
+    // const password = formData.get("password") as string;
+
+    const values: FormLogin = {
+      username: usernameRef.current?.value ?? "",
+      password: passwordRef.current?.value ?? "",
+    };
+
+    const validation = validate(values);
+
+    if (Object.keys(validation).length > 0) {
+      setError(validation);
+      return;
+    }
+
+    setError({});
+    console.log(values);
   };
 
   return (
@@ -16,18 +64,20 @@ const LoginPage = () => {
           <h3 className="text-3xl font-semibold mb-4">Login</h3>
           <p className="text-sm text-gray-600">Silahkan login dengan akun yang terdaftar</p>
         </div>
-        <form onSubmit={handleOnSubmit}>
+        <form onSubmit={handleOnSubmit} autoComplete="off">
           <div className="mb-4">
             <label htmlFor="username" className="block text-sm font-medium text-gray-700">
               Username
             </label>
-            <input type="text" id="username" name="username" required className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600" />
+            <input type="text" id="username" ref={usernameRef} name="username" className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600" />
+            {error.username && <p className="text-red-600 text-xs mt-1">{error.username}</p>}
           </div>
           <div className="mb-6">
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">
               Password
             </label>
-            <input type="password" id="password" name="password" required className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600" />
+            <input type="password" id="password" ref={passwordRef} name="password" className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600" />
+            {error.password && <p className="text-red-600 text-xs mt-1">{error.password}</p>}
           </div>
 
           <button type="submit" className="px-4 py-2 mb-8 w-full border-none bg-blue-600 text-white font-medium cursor-pointer hover:bg-blue-700 hover:scale-105 transition-all duration-300">
--- my-react-typescript-app/src/components/Register.tsx
+++ my-react-typescript-app/src/components/Register.tsx
@@ -1,47 +1,52 @@
 import { useState } from "react";
 import { Link } from "react-router";
 
-type ErrorMessage = {
-  nama?: string;
-  username?: string;
-  email?: string;
-  password?: string;
-  confirmPassword?: string;
+type FormRegister = {
+  nama: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 };
 
-const validation = (nama: string, username: string, email: string, password: string, confirmPassword: string): ErrorMessage => {
+type ErrorMessage = Partial<FormRegister>;
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const validation = (errorsMsg: FormRegister): ErrorMessage => {
   const errors: ErrorMessage = {};
 
-  if (!nama) {
+  if (!errorsMsg.nama) {
     errors.nama = "Nama lengkap wajib diisi!";
-  } else if (nama.length < 3) {
+  } else if (errorsMsg.nama.length < 3) {
     errors.nama = "Nama lengkap minimal 3 karakter!";
-  } else if (nama.length > 100) {
-    errors.nama = "Nama lengkap maksimal 100 karakter!";
+  } else if (errorsMsg.nama.length > 50) {
+    errors.nama = "Nama lengkap maksimal 50 karakter!";
   }
 
-  if (!username) {
+  if (!errorsMsg.username) {
     errors.username = "Username wajib diisi!";
-  } else if (username.length < 3) {
+  } else if (errorsMsg.username.length < 3) {
     errors.username = "Username minimal 3 karakter!";
+  } else if (errorsMsg.username.length > 20) {
+    errors.username = "Username maksimal 20 karakter!";
   }
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!email) {
+  if (!errorsMsg.email) {
     errors.email = "Email wajib diisi!";
-  } else if (!emailRegex.test(email)) {
+  } else if (!emailRegex.test(errorsMsg.email)) {
     errors.email = "Format email tidak valid!";
   }
 
-  if (!password) {
+  if (!errorsMsg.password) {
     errors.password = "Password wajib diisi!";
-  } else if (password.length < 6) {
+  } else if (errorsMsg.password.length < 6) {
     errors.password = "Password minimal 6 karakter!";
   }
 
-  if (!confirmPassword) {
+  if (!errorsMsg.confirmPassword) {
     errors.confirmPassword = "Konfirmasi password wajib diisi!";
-  } else if (password !== confirmPassword) {
+  } else if (errorsMsg.confirmPassword !== errorsMsg.password) {
     errors.confirmPassword = "Password tidak sama!";
   }
 
@@ -50,17 +55,26 @@ const validation = (nama: string, username: string, email: string, password: str
 
 const RegisterPage = () => {
   const [errors, setErrors] = useState<ErrorMessage>({});
+  const [user, setUser] = useState<FormRegister>({
+    nama: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  console.log(user);
 
   const handleSubmited = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const nama = formData.get("nama") as string;
-    const username = formData.get("username") as string;
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const confirmPassword = formData.get("confirm_password") as string;
+    // const formData = new FormData(e.currentTarget);
+    // const nama = formData.get("nama") as string;
+    // const username = formData.get("username") as string;
+    // const email = formData.get("email") as string;
+    // const password = formData.get("password") as string;
+    // const confirmPassword = formData.get("confirm_password") as string;
 
-    const validateError = validation(nama, username, email, password, confirmPassword);
+    const validateError = validation(user);
 
     if (Object.keys(validateError).length > 0) {
       setErrors(validateError);
@@ -68,12 +82,7 @@ const RegisterPage = () => {
     }
 
     setErrors({});
-    console.log({
-      nama,
-      username,
-      email,
-      password,
-    });
+    console.log(user);
   };
 
   return (
@@ -88,35 +97,70 @@ const RegisterPage = () => {
             <label htmlFor="nama" className="block text-sm font-medium text-gray-700">
               Nama Lengkap
             </label>
-            <input type="text" id="nama" name="nama" className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600" />
+            <input
+              type="text"
+              id="nama"
+              name="nama"
+              onChange={(e) => setUser({ ...user, nama: e.target.value })}
+              value={user.nama}
+              className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600"
+            />
             {errors.nama && <p className="text-red-500 text-xs mt-1">{errors.nama}</p>}
           </div>
           <div className="mb-4">
             <label htmlFor="username" className="block text-sm font-medium text-gray-700">
               Username
             </label>
-            <input type="text" id="username" name="username" className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600" />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+              value={user.username}
+              className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600"
+            />
             {errors.username && <p className="text-red-500 text-xs mt-1">{errors.username}</p>}
           </div>
           <div className="mb-4">
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
               Email
             </label>
-            <input type="text" id="email" name="email" className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              value={user.email}
+              className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600"
+            />
             {errors.email && <p className="text-red-500 text-xs mt-1">{errors.email}</p>}
           </div>
           <div className="mb-4">
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">
               Password
             </label>
-            <input type="password" id="password" name="password" className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              value={user.password}
+              className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600"
+            />
             {errors.password && <p className="text-red-500 text-xs mt-1">{errors.password}</p>}
           </div>
           <div className="mb-6">
-            <label htmlFor="confirm_password" className="block text-sm font-medium text-gray-700">
+            <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
               Konfirmasi Password
             </label>
-            <input type="password" id="confirm_password" name="confirm_password" className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600" />
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              onChange={(e) => setUser({ ...user, confirmPassword: e.target.value })}
+              value={user.confirmPassword}
+              className="px-3 py-1.5 mt-1 w-full border border-gray-300 rounded-lg focus:outline-1 focus:outline-blue-600 focus:border-blue-600"
+            />
             {errors.confirmPassword && <p className="text-red-500 text-xs mt-1">{errors.confirmPassword}</p>}
           </div>
 
